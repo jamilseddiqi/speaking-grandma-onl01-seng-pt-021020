bc9d364dd2e3eb84fb33ba108a6b96a40041a511
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-16 19:13:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,7 +23,7 @@ def speak_to_grandma(phrase)
       return "NO, NOT SINCE 1938!"
     elsif phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-  	else "NO, NOT SINCE 1938!"
+  # 	else "NO, NOT SINCE 1938!"
   	end 
 
 end
